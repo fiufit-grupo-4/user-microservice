cfbeb4f5c516dd6d836db974d1ed44b22b7e34b8
Federico Pacheco <fpacheco@fi.uba.ar>
2023-03-29 01:09:22 -0300

    linter added.
    Co-authored-by: Martina Agata <mpanetta@fi.uba.ar>

--- app/controller_example.py
+++ app/controller_example.py
@@ -2,8 +2,7 @@ from fastapi import APIRouter
 
 router = APIRouter(tags=["example"])
 
+
 @router.get("/example")
 async def get_example() -> dict:
-    return {
-        "message": "soy un ejemplo"
-    }
+    return {"message": "soy un ejemplo"}
--- app/main.py
+++ app/main.py
@@ -4,23 +4,25 @@ import pymongo
 
 app = FastAPI()
 
+
 @app.get("/", tags=["Home"])
 def get_root() -> dict:
-    return {
-        "message": "OK"
-    }
-    
+    return {"message": "OK"}
+
+
 @app.on_event("startup")
 async def startup_db_client():
     try:
         app.mongodb_client = pymongo.MongoClient("mongodb:27017")
         print("Connected successfully!")
-    except:  
+    except Exception as e:  # !!!
+        print(e)
         print("Could not connect to MongoDB")
-  
+
 
 @app.on_event("shutdown")
 async def shutdown_db_client():
     app.mongodb_client.close()
-    
+
+
 app.include_router(example_router)
