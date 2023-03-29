fb43dadf88a80d9be56da3dd9c38855a6f3bd1aa
Federico Pacheco <fpacheco@fi.uba.ar>
2023-03-29 00:09:01 -0300

    pytest fixed.
    Co-authored-by: Martina Agata <mpanetta@fi.uba.ar>

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,12 +1,10 @@
-#from fastapi import FastAPI
-#from fastapi.testclient import TestClient
-#from app.main import app
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+from app.main import app
 
-#client = TestClient(app)
-
+client = TestClient(app)
 
 def test_read_main():
-    assert 1 == 1
-    #response = client.get("/")
-    #assert response.status_code == 200
-    #assert response.json() == {"msg": "Hello World"}
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "OK"}
