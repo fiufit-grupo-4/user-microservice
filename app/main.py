bcbed64ff3fc63abacc52ce7ce6217e6f40abbd5
Federico Pacheco <fpacheco@fi.uba.ar>
2023-03-30 01:31:19 -0300

    logging added.
    Co-authored-by: Martina Agata <mpanetta@fi.uba.ar>

--- app/main.py
+++ app/main.py
@@ -1,28 +1,36 @@
 from fastapi import FastAPI
 from app.controller_example import router as example_router
 import pymongo
+import logging
+from logging.config import dictConfig
+from fastapi import FastAPI
+from .log_config import logconfig
 
+dictConfig(logconfig)
 app = FastAPI()
+logger = logging.getLogger('app')
 
+logger.error("Error message! - Level 3")
+logger.warning("Warning message! - Level 2")
+logger.info("Info message! - Level 1")
+logger.debug("Debug message! - Level 0")
 
 @app.get("/", tags=["Home"])
 def get_root() -> dict:
     return {"message": "OK"}
 
-
 @app.on_event("startup")
 async def startup_db_client():
     try:
         app.mongodb_client = pymongo.MongoClient("mongodb:27017")
-        print("Connected successfully!")
+        logger.info("Connected successfully MongoDB")
     except Exception as e:  # !!!
-        print(e)
-        print("Could not connect to MongoDB")
-
+        logger.error(e)
+        logger.error("Could not connect to MongoDB")
 
 @app.on_event("shutdown")
 async def shutdown_db_client():
     app.mongodb_client.close()
-
+    logger.info("Shutdown APP")
 
 app.include_router(example_router)
