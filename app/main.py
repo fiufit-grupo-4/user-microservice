5ce7331e7f7ef12fc45c5748d3814dbf3e8af704
LucasWaisten <waistenlucas@gmail.com>
2023-03-30 01:53:09 -0300

    Login de usuario FastApi

--- app/main.py
+++ app/main.py
@@ -6,9 +6,12 @@ from typing import Optional
 from starlette.responses import JSONResponse
 from app.user.user import *
 import pymongo
+from fastapi import FastAPI, HTTPException
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from passlib.context import CryptContext
 
 
-app = FastAPI(tags=["user-microservice"])
+app = FastAPI()
 users = {}
 
 
@@ -99,3 +102,38 @@ async def update_users(user_id: str, update_user_request: UpdateUserRequest):
     user.mail = update_user_request.mail
     users[user_id] = user
     return user
+
+### LOGIN USER ###
+
+security = HTTPBasic()
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+users_login = {
+    "foo": {
+        "username": "foo",
+        "full_name": "Lucas",
+        "email": "ss@gmail.com",
+        "hashed_password": "pepe"
+    }
+}
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_user(username: str):
+    if username in users_login:
+        user_dict = users_login[username]
+        return user_dict
+    return None
+
+@app.post("/login", tags=["User-Microservice"])
+def login(credentials: HTTPBasicCredentials):
+    user = get_user(credentials.username)
+    print("\n USUARIO" + user)
+    if not user:
+        raise HTTPException(status_code=401, detail="Invalid username")
+    if not verify_password(credentials.password, user["hashed_password"]):
+        raise HTTPException(status_code=401, detail="Invalid password")
+    return {"username": user["username"], "email": user["email"]}
+
