904b75f59076eac3edb22157e67a552840411c55
Martina Ágata <mpanetta@fi.uba.ar>
2023-04-01 00:41:04 -0300

    Mongodb integrated and use examples
    Co-Authored-By: Federico Jose Pacheco <51347305+fjpacheco@users.noreply.github.com>

--- app/main.py
+++ app/main.py
@@ -4,6 +4,9 @@ import pymongo
 import logging
 from logging.config import dictConfig
 from .log_config import logconfig
+from os import environ
+
+MONGODB_URI = environ["MONGODB_URI"]
 
 dictConfig(logconfig)
 app = FastAPI()
@@ -23,12 +26,28 @@ def get_root() -> dict:
 @app.on_event("startup")
 async def startup_db_client():
     try:
-        app.mongodb_client = pymongo.MongoClient("mongodb:27017")
+        app.mongodb_client = pymongo.MongoClient(MONGODB_URI)
         logger.info("Connected successfully MongoDB")
-    except Exception as e:  # !!!
+
+    except Exception as e:
         logger.error(e)
         logger.error("Could not connect to MongoDB")
 
+    # How to build a collection
+    db = app.mongodb_client["example-db"]
+    collection = db.example_collection
+
+    collection.delete_many({}) # Clear collection data
+
+    # Add data to collection
+    person_1 = {"name":"Agustin","age":{"$numberInt":"99"}}
+    logger.info("Added object with id: %s", collection.insert_one(person_1).inserted_id)
+    person_2 = {"name":"Alfonso","age":{"$numberInt":"10"}}
+    logger.info("Added object with id: %s", collection.insert_one(person_2).inserted_id)
+
+    # Check all data in collection
+    for p in collection.find():
+        logger.warning(p)
 
 @app.on_event("shutdown")
 async def shutdown_db_client():
