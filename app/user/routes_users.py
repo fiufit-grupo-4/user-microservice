48a0bb5651cff5c8b068f59a1a4136bc3b026d6a
feijooso <sfeijoo@fi.uba.ar>
2023-05-04 01:23:40 -0300

    /me endpoint

--- app/user/routes_users.py
+++ app/user/routes_users.py
@@ -1,10 +1,13 @@
 import logging
 from bson import ObjectId
-from fastapi import APIRouter, Depends, Query, Request
+from fastapi import APIRouter, Depends, Query, Request, HTTPException
 from passlib.context import CryptContext
 from starlette import status
 from starlette.responses import JSONResponse
 from typing import List
+from app.settings.auth_settings import JWT_SECRET
+from app.settings.auth_baerer import JWTBearer
+import jwt
 
 from app.user.user import (
     QueryParamFilterUser,
@@ -19,20 +22,21 @@ router = APIRouter()
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
-def fail_if_no_changes(changes):
-    if not changes or len(changes) == 0:
-        logger.info('No values specified in body to update')
-        return JSONResponse(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            content='No values specified to update',
+def get_user_id(token: str = Depends(JWTBearer())) -> ObjectId:
+    try:
+        token_data_user = jwt.decode(token, JWT_SECRET, algorithms=["HS256"])
+        return ObjectId(token_data_user["id"])
+    except Exception:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid token"
         )
 
 
 @router.get('/', response_model=List[UserResponse], status_code=status.HTTP_200_OK)
 async def get_users(
-        request: Request,
-        queries: QueryParamFilterUser = Depends(),
-        limit: int = Query(128, ge=1, le=1024),
+    request: Request,
+    queries: QueryParamFilterUser = Depends(),
+    limit: int = Query(128, ge=1, le=1024),
 ):
     users = request.app.database["users"]
 
@@ -44,6 +48,22 @@ async def get_users(
         f'Return list of {len(user_list)} users, with query params: {queries.dict(exclude_none=True)}'
     )
     return user_list
+
+
+@router.get('/me', response_model=UserResponse, status_code=status.HTTP_200_OK)
+async def get_user(request: Request, user_id: ObjectId = Depends(get_user_id)):
+    users = request.app.database["users"]
+    user = users.find_one({"_id": user_id})
+
+    if user:
+        logger.info(f'Get a user {user_id}')
+        return UserResponse.from_mongo(user)
+    else:
+        logger.info(f'User {user_id} not found to get')
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content=f'User {user_id} not found to get',
+        )
 
 
 @router.get('/{user_id}', response_model=UserResponse, status_code=status.HTTP_200_OK)
@@ -64,7 +84,7 @@ async def get_user(request: Request, user_id: ObjectIdPydantic):
 
 @router.patch('/{user_id}', status_code=status.HTTP_200_OK)
 async def update_users(
-        request: Request, user_id: ObjectIdPydantic, update_user_request: UpdateUserRequest
+    request: Request, user_id: ObjectIdPydantic, update_user_request: UpdateUserRequest
 ):
     to_change = update_user_request.dict(exclude_none=True)
 
