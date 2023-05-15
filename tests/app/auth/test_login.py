134fda970fb6ee627df8c493abc2bc8d004762f0
feijooso <sfeijoo@fi.uba.ar>
2023-05-15 17:27:07 -0300

    test for blocked users login

--- tests/app/auth/test_login.py
+++ tests/app/auth/test_login.py
@@ -12,7 +12,11 @@ encrypted_password = '$2b$12$T3HXmxRONP1sjTkk3Pqaq.9IYl5KNRhMHyJC4QxZPx0AqJpctDq
 
 atleta = {"name": "lucas", "lastname": "martinez", "age": "20", "mail": "waistenlucas@gmail.com",
           "encrypted_password": encrypted_password, "session_token": "token", "role": 3,
-          "phone_number": '+5493446570174', "image": "image.png", "blocked":  False, "trainings": []}
+          "phone_number": '+5493446570174', "image": "image.png", "blocked": False, "trainings": []}
+
+blocked_user = {"name": "lucas", "lastname": "lopez", "age": "20", "mail": "lopezlucas@gmail.com",
+                "encrypted_password": encrypted_password, "session_token": "token", "role": 3,
+                "phone_number": '+5493446570174', "image": "image.png", "blocked": True, "trainings": []}
 
 
 # Mock MongoDB
@@ -70,6 +74,18 @@ def test_fail_if_wrong_role(mongo_mock):
         "password": password,
         "phone_number": atleta['phone_number'],
         "role": 1
+    }
+
+    response = client.post("/login/", json=credentials)
+    assert response.status_code == 401
+
+
+def test_fail_if_blocked(mongo_mock):
+    credentials = {
+        "mail": blocked_user['mail'],
+        "password": password,
+        "phone_number": blocked_user['phone_number'],
+        "role": 3
     }
 
     response = client.post("/login/", json=credentials)
