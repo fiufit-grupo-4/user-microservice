0902d4463c21e1b653ce779b9cc6cbb53d4f9d3c
feijooso <sfeijoo@fi.uba.ar>
2023-05-15 17:15:55 -0300

    login response

--- app/auth/login.py
+++ app/auth/login.py
@@ -1,14 +1,20 @@
 import logging
+from typing import Optional
+
 from dotenv import load_dotenv
 from fastapi import APIRouter
 from fastapi import Request, status
 from fastapi.responses import JSONResponse
+from pydantic import BaseModel, EmailStr
 
+from app.domain.UserRoles import UserRoles
 from app.settings.config import pwd_context
+from app.user.training_small import TrainingResponse
 from app.user.user import UserBasicCredentials
 from app.settings.auth_settings import generate_token
 from app.auth.password_reset import router as password_router
 from app.auth.google_login import router as google_login_router
+from app.user.utils import ObjectIdPydantic
 
 load_dotenv()
 logger = logging.getLogger("app")
@@ -20,6 +26,34 @@ router.include_router(
     prefix="",
     tags=["login"],
 )
+
+
+class LoginResponse:
+    def __init__(
+        self,
+        mail,
+        phone_number,
+        role,
+        name,
+        lastname,
+        age,
+        image,
+        blocked,
+        trainings,
+        access_token,
+        token_type
+    ):
+        self.name = name
+        self.lastname = lastname
+        self.age = age
+        self.mail = mail
+        self.role = role
+        self.image = image
+        self.blocked = blocked
+        self.phone_number = phone_number
+        self.trainings = trainings
+        self.access_token = access_token
+        self.token_type = token_type
 
 
 def is_password_valid(plain_password, hashed_password):
@@ -49,4 +83,6 @@ def login(credentials: UserBasicCredentials, request: Request):
     access_token = generate_token(str(user["_id"]), user["role"])
 
     request.app.logger.info(f"User logged in: {credentials.mail} | id: {user['_id']}")
-    return {"access_token": access_token, "token_type": "bearer"}
+
+    return LoginResponse(user["mail"], user["phone_number"], user["role"], user["name"], user["lastname"], user["age"], user["image"],
+                         user["blocked"], user["trainings"], access_token, "bearer")
