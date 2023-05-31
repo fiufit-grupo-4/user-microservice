72e151d14c100a233c499c7b2a55ef1bd7c6d633
Federico Pacheco <fpacheco@fi.uba.ar>
2023-05-30 21:38:17 -0300

    id in LoginResponse

--- tests/app/auth/test_login.py
+++ tests/app/auth/test_login.py
@@ -13,7 +13,7 @@ encrypted_password = '$2b$12$T3HXmxRONP1sjTkk3Pqaq.9IYl5KNRhMHyJC4QxZPx0AqJpctDq
 atleta = {"name": "lucas", "lastname": "martinez", "age": "20", "mail": "waistenlucas@gmail.com",
           "encrypted_password": encrypted_password, "session_token": "token", "role": 3,
           "phone_number": '+5493446570174', "image": "image.png", "blocked": False, "trainings": [],
-          "location": {"longitude": float(300), "latitude": float(400)}}
+          "location": {"longitude": float(300), "latitude": float(400)}, "id" : "1234"}
 
 blocked_user = {"name": "lucas", "lastname": "lopez", "age": "20", "mail": "lopezlucas@gmail.com",
                 "encrypted_password": encrypted_password, "session_token": "token", "role": 3,
@@ -36,6 +36,7 @@ def mongo_mock(monkeypatch):
 
 def test_succeed_if_credentials_are_correct(mongo_mock):
     credentials = {
+        "id": atleta['id'],
         "mail": atleta['mail'],
         "password": password,
         "role": 3
